beaf92efaf65bdbd90b62d34307539c93bf73cbe
ankita garg <ankitagarg462@gmail.com>
2020-05-16 17:08:53 +0530

    Init Commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=F:\fLUUTER_COURSE\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\om\Desktop\flutter\Clima-Flutter"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=F:\fLUUTER_COURSE\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
